65541f19a7f9bd0cf0a1d071e27c396c685a81fb
Kyle Parrish <110362534+KMParrish@users.noreply.github.com>
2022-10-18 14:06:36 -0500

    generated html template in place

--- src/generated html.js
+++ src/generated html.js
@@ -1,3 +1,33 @@
+const generateHTMLTemplate = function (employees) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <title>Document</title>
+  </head>
+  <body>
+      <body>
+          <header>
+              <h2 style="padding: 40px;" class="text-center text-white bg-danger">My Team</h2>
+                  <div class="container-fluid">
+          </header>
+          <main>
+              <div class="container">
+                  <div class="justify-content-center row">
+                      ${employees}
+                  </div>
+              </div>
+          </main>
+      
+  </body>
+  </html>`
+}
+
 const generateEngineerCard = function (engineer) {
   return `
   <div class="card col-2 center">
@@ -40,3 +70,35 @@ const generateInternCard = function (intern) {
   </div>
 </div>`
 }
+fillCards = (data) => {
+  pageArray = []; 
+
+  for (let i = 0; i < data.length; i++) {
+      const employee = data[i];
+      const role = employee.getRole(); 
+
+      if (role === 'Manager') {
+          const managerDiv = generateManagerCard(employee);
+          pageArray.push(managerDiv);
+      }
+
+      if (role === 'Engineer') {
+          const engineerDiv = generateEngineerCard(employee);
+          pageArray.push(engineerDiv);
+      }
+
+      if (role === 'Intern') {
+          const internDiv = generateInternCard(internDiv);
+          pageArray.push(internCard);
+      }
+      
+  }
+
+  const employees = pageArray.join('')
+  
+  const generateHTML = fillCards(employees); 
+  return generateHTML;
+
+}
+
+module.exports = fillCards; 
